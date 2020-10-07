059e73efe275482748387824c6cd9979ac412501
Zeeshan91490 <www.zeeshankhan91490@gmail.com>
2020-10-07 22:16:30 +0530

    moodanalyser/

--- moodanalyser/src/main/java/com/capgem/moodanalyser/MoodAnalyser.java
+++ moodanalyser/src/main/java/com/capgem/moodanalyser/MoodAnalyser.java
@@ -1,34 +1,23 @@
 package com.capgem.moodanalyser;
 
-enum ExceptionCause {
-	EMPTY, NULL;
-}
-
-public class MoodAnalyser {
-	private String message;
-
-	public MoodAnalyser() {
+public class uc1moodanalyser {
+private String message;
+	
+	public uc1moodanalyser() {
+		
 	}
 
-	public MoodAnalyser(String message) {
+	public uc1moodanalyser(String message) {
 		this.message = message;
 	}
-
-	public String analyseMood() throws MoodAnalyserException {
-		try {
-			if(message.contains("I am in Sad Mood"))
-				return "SAD";
-			else if(message.isBlank()) {
-				ExceptionCause causeOne = ExceptionCause.EMPTY;
-				throw new MoodAnalyserException("You can't have the message as " + causeOne);
-			}
-			else
-				return "HAPPY"; 
-		}
-		catch(NullPointerException e) {
-			ExceptionCause causeTwo = ExceptionCause.NULL;
-			throw new MoodAnalyserException("You can't have the message as " + causeTwo);
-		}
+	
+	public String analyseMood() {
+		if(message.contains("I am in Sad Mood"))
+			return "SAD";
+		else
+			return "HAPPY"; 
 	}
 	public static void main( String[] args ) {
-	}}
+		
+	}
+}
